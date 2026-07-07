b2c727318ba39971ba0009f4d2ac624f27e9b956
Lenovo <1070892641@qq.com>
2026-07-07 11:34:03 +0800

    clean python cache files

---
+++ test.py
@@ -0,0 +1,14 @@
+{'level_control':
+     {'low_level': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame.!frame.!frame2.!entry>,
+'target_level': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame2.!frame.!frame2.!entry>,
+'upper_level': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame3.!frame.!frame2.!entry>,
+'level_high_high': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame4.!frame.!frame2.!entry>,
+'rise_rate_trigger': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame5.!frame.!frame2.!entry>,
+'fall_rate_trigger': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame6.!frame.!frame2.!entry>,
+'freq_min': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame7.!frame.!frame2.!entry>,
+'freq_economic': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame8.!frame.!frame2.!entry>,
+'freq_normal': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame9.!frame.!frame2.!entry>,
+'freq_high': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame10.!frame.!frame2.!entry>,
+'feed_start_delay_seconds': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame11.!frame.!frame2.!entry>,
+'start_delay_seconds': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame12.!frame.!frame2.!entry>,
+'feed_stop_delay_seconds': <tkinter.Entry object .!notebook.!frame9.!frame.!frame2.!frame2.!frame.!frame.!frame.!frame13.!frame.!frame2.!entry>}}
